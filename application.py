89dc2a791d66a8278bffd6892af7c8d37e5b3464
Khushi Jain <beingkhushi22@gmail.com>
2025-11-25 18:10:52 +0530

    uppade the file to deploy it to render

--- application.py
+++ application.py
@@ -48,4 +48,6 @@ def predict_datapoint():
         return render_template("home.html")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
